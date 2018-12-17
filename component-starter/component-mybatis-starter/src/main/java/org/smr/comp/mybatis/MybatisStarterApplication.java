163925ca805d19e87512c1b75207e2f04f776e07
blade <97039144@qq.com>
2018-12-17 16:49:24 +0800

    mybatis组件调整

---
+++ component-starter/component-mybatis-starter/src/main/java/org/smr/comp/mybatis/MybatisStarterApplication.java
@@ -0,0 +1,23 @@
+package org.smr.comp.mybatis;
+/*
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class MybatisStarterApplication {
+
+
+        public static void main(String[] args) {
+            new SpringApplicationBuilder(MybatisStarterApplication.class).web(true).run(args);
+        }
+
+        public int getExitCode() {
+            System.out.println(getClass().getName() + " is exit!");
+            return 0;
+        }
+
+}
+*/
---
+++ component-starter/component-mybatis-starter/src/main/java/org/smr/comp/mybatis/conf/MyBatisProperties.java
@@ -0,0 +1,81 @@
+package org.smr.comp.mybatis.conf;
+
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ExecutorType;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+
+@org.springframework.context.annotation.Configuration
+@ConfigurationProperties(prefix = "mybatis")
+public class MyBatisProperties {
+
+   private String mapperLocations;
+   private String typeAliasesPackage;
+
+    private boolean checkConfigLocation = false;
+
+    /**
+     * Execution mode for {@link org.mybatis.spring.SqlSessionTemplate}.
+     */
+    private ExecutorType executorType;
+
+
+    /**
+     * A Configuration object for customize default settings. If {@link #configLocation}
+     * is specified, this property is not used.
+     */
+    @NestedConfigurationProperty
+    private Configuration configuration;
+    /**
+     * Location of MyBatis xml config file.
+     */
+    private String configLocation;
+
+    public String getConfigLocation() {
+        return configLocation;
+    }
+
+    public void setConfigLocation(String configLocation) {
+        this.configLocation = configLocation;
+    }
+
+    public String getMapperLocations() {
+        return mapperLocations;
+    }
+
+    public void setMapperLocations(String mapperLocations) {
+        this.mapperLocations = mapperLocations;
+    }
+
+    public String getTypeAliasesPackage() {
+        return typeAliasesPackage;
+    }
+
+    public void setTypeAliasesPackage(String typeAliasesPackage) {
+        this.typeAliasesPackage = typeAliasesPackage;
+    }
+
+    public boolean isCheckConfigLocation() {
+        return checkConfigLocation;
+    }
+
+    public void setCheckConfigLocation(boolean checkConfigLocation) {
+        this.checkConfigLocation = checkConfigLocation;
+    }
+
+    public Configuration getConfiguration() {
+        return configuration;
+    }
+
+    public void setConfiguration(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    public ExecutorType getExecutorType() {
+        return executorType;
+    }
+
+    public void setExecutorType(ExecutorType executorType) {
+        this.executorType = executorType;
+    }
+}
